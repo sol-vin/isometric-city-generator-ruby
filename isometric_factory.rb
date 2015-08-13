d78fcecffdbde86c4a9fdb12d42ed6545fd62130
Ian Rash <redcodefinal@gmail.com>
2015-08-13 00:57:23 -0700

    Added comments

--- isometric_factory.rb
+++ isometric_factory.rb
@@ -6,17 +6,22 @@ class IsometricFactory
   OFFSET = Point.new(0, 0)
   MAX_HEIGHT = 6
 
+  #Hash of direction keys, which way the camera is facing and what direction is facing clockwise
   VIEWS = {west_south: :north_west, north_west: :east_north, east_north: :south_east, south_east: :west_south}
 
+  #Size of the landscape
   attr_reader :size_x, :size_y
+  #current camera direction
   attr_reader :view
 
+  #gets the actual position of a tile based on its index
   def self.get_tile_position(x, y)
     spacing = Point.new((Assets.tile_width/2.0).round, (Assets.tile_height/2.0).round)
     Point.new((-x * spacing.x) + (y * spacing.x) - y + x + OFFSET.x,
               (x * spacing.y) + (y*spacing.y) - y - x + OFFSET.y)
   end
-
+  
+  #gets the actual position of a block based on its index
   def self.get_block_position(x, y, z)
     position = get_tile_position(x,y)
     position.y -= (Assets.block_height / 2.0).round * (z + 1)
@@ -29,18 +34,22 @@ class IsometricFactory
     @view = :north_west
   end
 
+  #finds out what kind of tile is at an index
   def get_tile_type(x, y)
     :tile
   end
 
+  #finds out what kind of block is at an index
   def get_block_type(x, y, z)
     (is_block_at?(x, y ,z) ? :block : nil)
   end
-
+  
+  #is there a block at index?
   def is_block_at(x, y, z)
     true
   end
-
+  
+  #draw phase for the grid
   def draw_grid
     size_x.times do |x|
       size_y.times do |y|
@@ -50,23 +59,28 @@ class IsometricFactory
     end
   end
 
+  #draw a single block
   def draw_block(x_pos, y_pos, x, y, z, color)
     position = IsometricFactory.get_block_position(x_pos, y_pos, z)
     Assets.get_block_asset(get_block_type(x, y, z)).draw(position, 1, color)
   end
 
+  #draw all the blocks
   def draw_blocks
-
+    
   end
 
+  #rotate the view counter clockwise
   def rotate_counter_clockwise
     @view = VIEWS[view]
   end
 
+  #rotate the view clockwise
   def rotate_clockwise
     @view = VIEWS.invert[view]
   end
-
+  
+  #find the angle for drawing the blocks 
   def get_view_ranges
     x_ranges = {east: {start: size_x, end: 0}, west: {start: 0, end: size_x}}
     y_ranges = {north:  {start: size_y, end: 0}, south:  {start: 0, end: size_y}}
